b0e4272a7d110513d5565bef839bd234ba56b82c
otabeck76 <uotkirovo58@gmail.com>
2024-05-04 19:18:00 +0500

    3rd tasl

--- tst= int(input('give me numbers') ).py
+++ tst= int(input('give me numbers') ).py
@@ -12,8 +12,19 @@
 #     print(False)
 
 #2 task
-tst=15
-if 10<tst<20:
-    print('tst больше 10')
+# tst=15
+# if 10<tst<20:
+#     print('tst больше 10')
+# else:
+#     print('меньше')
+
+# tst=-5
+# if tst>0<-10 and tst<-8<-30:
+#     print(True)
+# else:
+#     print(False)
+tst=['a','b','c','d','e']
+if 6> len(tst)<0:
+    print(True)
 else:
-    print('меньше')
+    print(False)
